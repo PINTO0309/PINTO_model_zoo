0aa672de8e03057e2f3bef59696c2c02a1a2beb7
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-01-02 22:15:26 +0900

    update

--- 03_posenet/01_float32/06_float16_quantization.py
+++ 03_posenet/01_float32/06_float16_quantization.py
@@ -7,14 +7,14 @@ import glob
 
 tf.compat.v1.enable_eager_execution()
 
-## Float16 Quantization - Input/Output=float32
-#converter = tf.lite.TFLiteConverter.from_saved_model('./0')
-#converter.optimizations = [tf.lite.Optimize.DEFAULT]
-#converter.target_spec.supported_types = [tf.lite.constants.FLOAT16]
-#tflite_quant_model = converter.convert()
-#with open('./model-mobilenet_v1_101_225_float16_quant.tflite', 'wb') as w:
-#    w.write(tflite_quant_model)
-#print("Integer Quantization complete! - model-mobilenet_v1_101_225_float16_quant.tflite")
+# Float16 Quantization - Input/Output=float32
+converter = tf.lite.TFLiteConverter.from_saved_model('./0')
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_types = [tf.lite.constants.FLOAT16]
+tflite_quant_model = converter.convert()
+with open('./model-mobilenet_v1_101_225_float16_quant.tflite', 'wb') as w:
+    w.write(tflite_quant_model)
+print("Integer Quantization complete! - model-mobilenet_v1_101_225_float16_quant.tflite")
 
 ## Float16 Quantization - Input/Output=float32
 #converter = tf.lite.TFLiteConverter.from_saved_model('./0')
@@ -43,11 +43,11 @@ tf.compat.v1.enable_eager_execution()
 #    w.write(tflite_quant_model)
 #print("Integer Quantization complete! - model-mobilenet_v1_101_385_float16_quant.tflite")
 
-# Float16 Quantization - Input/Output=float32
-converter = tf.lite.TFLiteConverter.from_saved_model('./0')
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.target_spec.supported_types = [tf.lite.constants.FLOAT16]
-tflite_quant_model = converter.convert()
-with open('./model-mobilenet_v1_101_513_float16_quant.tflite', 'wb') as w:
-    w.write(tflite_quant_model)
-print("Integer Quantization complete! - model-mobilenet_v1_101_513_float16_quant.tflite")
+## Float16 Quantization - Input/Output=float32
+#converter = tf.lite.TFLiteConverter.from_saved_model('./0')
+#converter.optimizations = [tf.lite.Optimize.DEFAULT]
+#converter.target_spec.supported_types = [tf.lite.constants.FLOAT16]
+#tflite_quant_model = converter.convert()
+#with open('./model-mobilenet_v1_101_513_float16_quant.tflite', 'wb') as w:
+#    w.write(tflite_quant_model)
+#print("Integer Quantization complete! - model-mobilenet_v1_101_513_float16_quant.tflite")
