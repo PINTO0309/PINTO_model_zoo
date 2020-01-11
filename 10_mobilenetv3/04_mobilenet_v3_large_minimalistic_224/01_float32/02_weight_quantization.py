063ce107e8dbb60a0d25566d605d63707876e9e3
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-01-11 22:58:29 +0900

    mobilenetv3 added

---
+++ 10_mobilenetv3/04_mobilenet_v3_large_minimalistic_224/01_float32/02_weight_quantization.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+
+tf.compat.v1.enable_eager_execution()
+
+# Weight Quantization - Input/Output=float32
+converter = tf.lite.TFLiteConverter.from_saved_model('./saved_model')
+converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+tflite_quant_model = converter.convert()
+with open('./mobilenet_v3_large_minimalistic_224_dm10_weight_quant.tflite', 'wb') as w:
+    w.write(tflite_quant_model)
+print("Weight Quantization complete! - mobilenet_v3_large_minimalistic_224_dm10_weight_quant.tflite")
+
---
+++ 10_mobilenetv3/04_mobilenet_v3_large_minimalistic_224/01_float32/04_full_integer_quantization.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+
+def representative_dataset_gen():
+  for data in raw_test_data.take(100):
+    image = data['image'].numpy()
+    image = tf.image.resize(image, (224, 224))
+    image = image[np.newaxis,:,:,:]
+    yield [image]
+
+
+tf.compat.v1.enable_eager_execution()
+
+raw_test_data, info = tfds.load(name="coco/2017", with_info=True, split="test", data_dir="~/TFDS", download=False)
+
+# Integer Quantization - Input/Output=float32
+converter = tf.lite.TFLiteConverter.from_saved_model('./saved_model')
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_dataset_gen
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+tflite_quant_model = converter.convert()
+with open('./mobilenet_v3_large_minimalistic_224_dm10_full_integer_quant.tflite', 'wb') as w:
+    w.write(tflite_quant_model)
+print("Integer Quantization complete! - mobilenet_v3_large_minimalistic_224_dm10_full_integer_quant.tflite")
