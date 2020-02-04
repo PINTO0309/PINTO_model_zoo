6805bb9e525937a3262e835b5e97db5cca0ea4d2
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-02-04 23:36:20 +0900

    voc added

--- 06_mobilenetv2-ssdlite/02_voc/mobilenetv2ssdlite.py
+++ 06_mobilenetv2-ssdlite/02_voc/mobilenetv2ssdlite.py
@@ -9,19 +9,12 @@ except:
     from tensorflow.lite.python.interpreter import Interpreter
 
 LABELS = [
-'person','bicycle','car','motorcycle','airplane','bus','train','truck','boat','traffic light',
-'fire hydrant','???','stop sign','parking meter','bench','bird','cat','dog','horse','sheep',
-'cow','elephant','bear','zebra','giraffe','???','backpack','umbrella','???','???',
-'handbag','tie','suitcase','frisbee','skis','snowboard','sports ball','kite','baseball bat','baseball glove',
-'skateboard','surfboard','tennis racket','bottle','???','wine glass','cup','fork','knife','spoon',
-'bowl','banana','apple','sandwich','orange','broccoli','carrot','hot dog','pizza','donut',
-'cake','chair','couch','potted plant','bed','???','dining table','???','???','toilet',
-'???','tv','laptop','mouse','remote','keyboard','cell phone','microwave','oven','toaster',
-'sink','refrigerator','???','book','clock','vase','scissors','teddy bear','hair drier','toothbrush']
+'background','aeroplane','bicycle','bird','boat','bottle','bus','car','cat','chair',
+'cow','diningtable','dog','horse','motorbike','person','pottedplant','sheep','sofa','train','tvmonitor']
 
 if __name__ == '__main__':
     image = cv2.imread('dog.jpg')
-    interpreter = Interpreter(model_path='03_integer_quantization/ssdlite_mobilenet_v2_coco_300_integer_quant_with_postprocess.tflite')
+    interpreter = Interpreter(model_path='03_integer_quantization/ssdlite_mobilenet_v2_voc_300_integer_quant_with_postprocess.tflite')
     try:
         interpreter.set_num_threads(4)
     except:
