6c4575facf1adb4a11ceb0c53525ad250ed86ebc
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-02-08 10:21:44 +0900

    update

---
+++ 01_deeplabv3/03_mobilenetv2_dm05/02_coco_voc_trainval/04_full_integer_quantization.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+
+def representative_dataset_gen():
+  for data in raw_test_data.take(10):
+    image = data['image'].numpy()
+    image = tf.image.resize(image, (513, 513))
+    image = image[np.newaxis,:,:,:]
+    yield [image]
+
+
+tf.compat.v1.enable_eager_execution()
+
+raw_test_data, info = tfds.load(name="voc/2007", with_info=True, split="validation", data_dir="~/TFDS", download=False)
+
+graph_def_file="frozen_inference_graph.pb"
+input_arrays=["ImageTensor"]
+output_arrays=['ArgMax']
+input_tensor={"ImageTensor":[1,513,513,3]}
+
+# Integer Quantization - Input/Output=float32
+#converter = tf.lite.TFLiteConverter.from_saved_model('./saved_model')
+converter = tf.lite.TFLiteConverter.from_frozen_graph(graph_def_file, input_arrays, output_arrays,input_tensor)
+converter.experimental_new_converter = True
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_dataset_gen
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.uint8
+converter.inference_output_type = tf.uint8
+tflite_quant_model = converter.convert()
+with open('./deeplabv3_mnv2_pascal_trainval_513_full_integer_quant.tflite', 'wb') as w:
+    w.write(tflite_quant_model)
+print("Integer Quantization complete! - deeplabv3_mnv2_pascal_trainval_513_full_integer_quant.tflite")
