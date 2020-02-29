260632ba48e7ff8373fe4912351ff5f5086340a2
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-02-29 20:16:20 +0900

    weight quantization model added

---
+++ 08_mask_rcnn_inceptionv2/01_float32/03_weight_quantization.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+### Tensorflow v2.1.0 - master - commit 8fdb834931fe62abaeab39fe6bf0bcc9499b25bf
+
+# Weight Quantization - Input/Output=float32
+converter = tf.lite.TFLiteConverter.from_saved_model('./saved_model')
+converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
+tflite_quant_model = converter.convert()
+with open('./mask_rcnn_inception_v2_coco_256_weight_quant.tflite', 'wb') as w:
+    w.write(tflite_quant_model)
+print("Weight Quantization complete! - mask_rcnn_inception_v2_coco_256_weight_quant.tflite")
+
