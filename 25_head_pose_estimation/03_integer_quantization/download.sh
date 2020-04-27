6f0f4d9a14039d9e88d3452616857c5f1d7179ab
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-04-27 21:58:09 +0900

    face detector added

--- 25_head_pose_estimation/03_integer_quantization/download.sh
+++ 25_head_pose_estimation/03_integer_quantization/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=u7Ez4HMLI5A5ite9eIH7JtG0ykJPeBo" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1lkZNPESdqQzBArwVnnZRs4dZmGRXdEi1" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=u7Ez4HMLI5A5ite9eIH7JtG0ykJPeBo" -o resources.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1lkZNPESdqQzBArwVnnZRs4dZmGRXdEi1" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
