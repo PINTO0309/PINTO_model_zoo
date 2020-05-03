1ed81aeb0cfeb94dddb84085a07dfec386ae496d
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-05-03 14:36:30 +0900

    Update 04_full_integer_quantization.py

--- 29_human-pose-estimation-3d-0001/01_float32/04_full_integer_quantization.py
+++ 29_human-pose-estimation-3d-0001/01_float32/04_full_integer_quantization.py
@@ -15,7 +15,7 @@ tf.compat.v1.enable_eager_execution()
 
 raw_test_data = np.load('calibration_data_img.npy', allow_pickle=True)
 
-# Integer Quantization - Input/Output=float32
+# Full Integer Quantization - Input/Output=float32
 converter = tf.lite.TFLiteConverter.from_saved_model('saved_model')
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.inference_input_type = tf.uint8
@@ -24,4 +24,4 @@ converter.representative_dataset = representative_dataset_gen
 tflite_quant_model = converter.convert()
 with open('human_pose_estimation_3d_0001_256x448_full_integer_quant.tflite', 'wb') as w:
     w.write(tflite_quant_model)
-print("Integer Quantization complete! - human_pose_estimation_3d_0001_256x448_full_integer_quant.tflite")
+print("Full Integer Quantization complete! - human_pose_estimation_3d_0001_256x448_full_integer_quant.tflite")
