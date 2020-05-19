df4c9d43bb5f9a0850b0028f6f17cb0bcca82bec
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-05-20 01:12:58 +0900

    Update 03_palm_detection_builtin_tflite_to_pb.py

--- 33_Hand_Detection_and_Tracking/01_float32/03_palm_detection_builtin_tflite_to_pb.py
+++ 33_Hand_Detection_and_Tracking/01_float32/03_palm_detection_builtin_tflite_to_pb.py
@@ -209,7 +209,6 @@ def make_graph(ops, op_types, interpreter):
             # print('shape_detail: ', output_shape_detail)
             # print('output_detail: ', output_detail)
             weights_array = interpreter.get_tensor(weights_detail['index'])
-            # weights_array = np.transpose(weights_array, (1, 2, 3, 0))
             weights_array = np.transpose(weights_array, (1, 2, 0, 3))
             # print('weights_array.shape:', weights_array.shape)
             output_shape_array = interpreter.get_tensor(output_shape_detail['index'])
