3665af0698065b56c1c655fab5ea102d2dca386c
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-05-25 00:14:51 +0900

    Update 04_integer_quantization.py

--- 37_First_Neural_Style_Transfer/01_float32/04_integer_quantization.py
+++ 37_First_Neural_Style_Transfer/01_float32/04_integer_quantization.py
@@ -23,13 +23,3 @@ tflite_quant_model = converter.convert()
 with open('mosaic_224_integer_quant.tflite', 'wb') as w:
     w.write(tflite_quant_model)
 print("Integer Quantization complete! - mosaic_224_integer_quant.tflite")
-
-# # Integer Quantization - Input/Output=float32
-# converter = tf.lite.TFLiteConverter.from_saved_model('saved_model_cityscapes')
-# converter.optimizations = [tf.lite.Optimize.DEFAULT]
-# converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,tf.lite.OpsSet.SELECT_TF_OPS]
-# converter.representative_dataset = representative_dataset_gen_cityscapes
-# tflite_quant_model = converter.convert()
-# with open('struct2depth_128x416_cityscapes_depth_integer_quant.tflite', 'wb') as w:
-#     w.write(tflite_quant_model)
-# print("Integer Quantization complete! - struct2depth_128x416_cityscapes_depth_integer_quant.tflite")
