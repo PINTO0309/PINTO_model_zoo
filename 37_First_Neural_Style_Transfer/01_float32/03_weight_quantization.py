c5687b6138650d5ab4c76bb837a7136c7df9b1c2
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-05-25 00:14:28 +0900

    Update 03_weight_quantization.py

--- 37_First_Neural_Style_Transfer/01_float32/03_weight_quantization.py
+++ 37_First_Neural_Style_Transfer/01_float32/03_weight_quantization.py
@@ -10,12 +10,3 @@ tflite_quant_model = converter.convert()
 with open('mosaic_224_weight_quant.tflite', 'wb') as w:
     w.write(tflite_quant_model)
 print("Weight Quantization complete! - mosaic_224_weight_quant.tflite")
-
-# # Weight Quantization - Input/Output=float32
-# converter = tf.lite.TFLiteConverter.from_saved_model('saved_model_mosaic')
-# converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
-# converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,tf.lite.OpsSet.SELECT_TF_OPS]
-# tflite_quant_model = converter.convert()
-# with open('mosaic_224_weight_quant.tflite', 'wb') as w:
-#     w.write(tflite_quant_model)
-# print("Weight Quantization complete! - mosaic_224_weight_quant.tflite")
