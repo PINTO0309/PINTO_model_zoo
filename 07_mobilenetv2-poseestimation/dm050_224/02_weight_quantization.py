5f61aee7e6ad85f7ae3f8814ffdba604a738a5d9
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-06-10 23:28:49 +0900

    bug fixed, integer quant, dm=050

--- 07_mobilenetv2-poseestimation/dm050_224/02_weight_quantization.py
+++ 07_mobilenetv2-poseestimation/dm050_224/02_weight_quantization.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 converter = tf.lite.TFLiteConverter.from_saved_model('saved_model')
 converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
 tflite_quant_model = converter.convert()
-with open('mobilenet_v2_pose_368_432_dm050_weight_quant.tflite', 'wb') as w:
+with open('mobilenet_v2_pose_256_256_dm050_weight_quant.tflite', 'wb') as w:
     w.write(tflite_quant_model)
-print("Weight Quantization complete! - mobilenet_v2_pose_368_432_dm050_weight_quant.tflite")
+print("Weight Quantization complete! - mobilenet_v2_pose_256_256_dm050_weight_quant.tflite")
 
--- 07_mobilenetv2-poseestimation/dm050_224/03_integer_quantization.py
+++ 07_mobilenetv2-poseestimation/dm050_224/03_integer_quantization.py
@@ -1,28 +1,21 @@
 ### tensorflow==2.2.0
 
 import tensorflow as tf
-import tensorflow_datasets as tfds
 import numpy as np
-from PIL import Image
-import os
-import glob
 
 def representative_dataset_gen():
-  for data in raw_test_data.take(100):
-    image = data['image'].numpy()
-    image = tf.image.resize(image, (368, 432))
-    image = image[np.newaxis,:,:,:]
-    image = image - 127.5
-    image = image * 0.007843
-    yield [image]
+    for image in raw_test_data:
+        image = tf.image.resize(image, (256, 256))
+        image = image[np.newaxis,:,:,:]
+        yield [image]
 
-raw_test_data, info = tfds.load(name="coco/2017", with_info=True, split="test", data_dir="~/TFDS", download=False)
+raw_test_data = np.load('calibration_data_img_person.npy', allow_pickle=True)
 
 # Integer Quantization - Input/Output=float32
 converter = tf.lite.TFLiteConverter.from_saved_model('saved_model')
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 converter.representative_dataset = representative_dataset_gen
 tflite_quant_model = converter.convert()
-with open('mobilenet_v2_pose_368_432_dm050_integer_quant.tflite', 'wb') as w:
+with open('mobilenet_v2_pose_256_256_dm050_integer_quant.tflite', 'wb') as w:
     w.write(tflite_quant_model)
-print("Integer Quantization complete! - mobilenet_v2_pose_368_432_dm050_integer_quant.tflite")
+print("Integer Quantization complete! - mobilenet_v2_pose_256_256_dm050_integer_quant.tflite")
