231020a3e4e47ca32050d8e88df86c13a2e8931f
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-06-23 06:36:53 +0900

    Added a script for loading test models

---
+++ 18_EfficientDet/01_float32/02_tflite_load_test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+
+interpreter = tf.lite.Interpreter(model_path="efficientdet_d0_256x256_weight_quant.tflite")
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+print('input:', input_details)
+print('')
+print('output:', output_details)
+
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+interpreter.invoke()
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print('output_data.shape:', output_data.shape)
