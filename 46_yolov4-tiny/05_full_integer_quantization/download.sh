3340546bfb390ab2d39ad4075f5e1e4042a75cd8
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-07-17 01:13:50 +0900

    openvino added

--- 46_yolov4-tiny/05_full_integer_quantization/download.sh
+++ 46_yolov4-tiny/05_full_integer_quantization/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1zrSbeUhIM-aJo8v0rByM0cE0t-NadHNf" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1UPNnoH00xfOgCqfjJXuYyqT3B7r6sTyw" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1zrSbeUhIM-aJo8v0rByM0cE0t-NadHNf" -o resources.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1UPNnoH00xfOgCqfjJXuYyqT3B7r6sTyw" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
