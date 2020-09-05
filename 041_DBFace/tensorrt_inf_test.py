8451ecb8a3df1c4bf900f19a13b33af00b81770c
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-09-05 09:20:59 +0900

    Change TF-TRT Converter and re-commit

--- 041_DBFace/tensorrt_inf_test.py
+++ 041_DBFace/tensorrt_inf_test.py
@@ -1,3 +1,5 @@
+### tensorflow==2.3.0
+
 ### https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/6.0/GA_6.0.1.5/local_repos/nv-tensorrt-repo-ubuntu1804-cuda10.1-trt6.0.1.5-ga-20190913_1-1_amd64.deb
 
 # os="ubuntu1804"
@@ -13,21 +15,41 @@
 # sudo apt-get install uff-converter-tf
 # sudo apt-get install onnx-graphsurgeon
 
-from tensorflow.python.compiler.tensorrt import trt_convert as trt
-
-converter = trt.TrtGraphConverterV2(input_saved_model_dir='saved_model_keras_480x640',
-                                    conversion_params=trt.DEFAULT_TRT_CONVERSION_PARAMS)
-converter.convert()
-converter.save('dbface_keras_480x640_tensorrt_float32_nhwc')
-
-converter = trt.TrtGraphConverterV2(input_saved_model_dir='saved_model_keras_480x640',
-                                    conversion_params=trt.DEFAULT_TRT_CONVERSION_PARAMS._replace(precision_mode=trt.TrtPrecisionMode.FP16))
-converter.convert()
-converter.save('dbface_keras_480x640_tensorrt_float16_nhwc')
-
 import tensorflow as tf
 import numpy as np
 import time
+
+# from tensorflow.python.compiler.tensorrt import trt_convert as trt
+
+# converter = trt.TrtGraphConverterV2(input_saved_model_dir='saved_model_keras_480x640',
+#                                     conversion_params=trt.DEFAULT_TRT_CONVERSION_PARAMS)
+# converter.convert()
+# converter.save('dbface_keras_480x640_tensorrt_float32_nhwc')
+
+# converter = trt.TrtGraphConverterV2(input_saved_model_dir='saved_model_keras_480x640',
+#                                     conversion_params=trt.DEFAULT_TRT_CONVERSION_PARAMS._replace(precision_mode=trt.TrtPrecisionMode.FP16))
+# converter.convert()
+# converter.save('dbface_keras_480x640_tensorrt_float16_nhwc')
+
+
+def input_fn():
+    input_shapes = [1, 480, 640, 3]
+    yield [np.zeros(input_shapes).astype(np.float32)]
+
+params = tf.experimental.tensorrt.ConversionParams(precision_mode='FP32', maximum_cached_engines=1000)
+converter = tf.experimental.tensorrt.Converter(input_saved_model_dir='saved_model_keras_480x640', conversion_params=params)
+converter.convert()
+converter.build(input_fn=input_fn)
+converter.save('dbface_keras_480x640_tensorrt_float32_nhwc')
+
+
+params = tf.experimental.tensorrt.ConversionParams(precision_mode='FP16', maximum_cached_engines=1000)
+converter = tf.experimental.tensorrt.Converter(input_saved_model_dir='saved_model_keras_480x640', conversion_params=params)
+converter.convert()
+converter.build(input_fn=input_fn)
+converter.save('dbface_keras_480x640_tensorrt_float16_nhwc')
+
+
 
 model = tf.saved_model.load('dbface_keras_480x640_tensorrt_float32_nhwc', tags=[tf.saved_model.SERVING])
 infer = model.signatures[tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
