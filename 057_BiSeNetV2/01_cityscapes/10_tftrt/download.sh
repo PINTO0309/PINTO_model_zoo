0dd9ad0f118fa7385a3370b13f98258d7ab950d7
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-09-06 14:27:46 +0900

    tf-trt added

---
+++ 057_BiSeNetV2/01_cityscapes/10_tftrt/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1w2IpOokFbkW1eXOhSmUP2Ap1JngdrfZ9" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1w2IpOokFbkW1eXOhSmUP2Ap1JngdrfZ9" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
