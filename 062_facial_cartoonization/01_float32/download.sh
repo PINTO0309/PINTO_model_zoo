9232421a0fca393da37c4d7923b46ddb2e260476
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-10-02 01:02:44 +0900

    facial cartoonization added, WIP

---
+++ 062_facial_cartoonization/01_float32/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1GTAHgC85qCVk-HRSP_n_E41M4DaBOvcZ" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1GTAHgC85qCVk-HRSP_n_E41M4DaBOvcZ" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
