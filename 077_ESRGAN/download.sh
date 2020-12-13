f8956876f98af3316a4c1194d7cc3521c24942e0
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2020-12-13 23:58:06 +0900

    ESRGAN added

---
+++ 077_ESRGAN/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1G_zgJQunEuLhuZ9h_-OFiozbWlnW1F_O" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1G_zgJQunEuLhuZ9h_-OFiozbWlnW1F_O" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
