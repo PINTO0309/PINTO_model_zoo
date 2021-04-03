06a178be741cff655526fa76176730ed8c6aa72b
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-04-03 20:00:01 +0900

    update

--- 078_MODNet/download.sh
+++ 078_MODNet/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1Y6B6l72hib9t5ammT-7ya-muBzRKZ7xq" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1bs0Tu0Tu_zIqp-ZCVNW1M6M8_c7kw4B3" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1Y6B6l72hib9t5ammT-7ya-muBzRKZ7xq" -o resources.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1bs0Tu0Tu_zIqp-ZCVNW1M6M8_c7kw4B3" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
