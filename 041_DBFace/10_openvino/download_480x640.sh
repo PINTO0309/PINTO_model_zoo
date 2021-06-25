f52936d35ba65eafcae8b0b69bfef783dba26f1b
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-06-25 11:21:07 +0900

    mobilenetv2, cleaning

--- 041_DBFace/10_openvino/download_480x640.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1tlJQKKCWURufsS93Y52-D52cNMAaxKOH" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1tlJQKKCWURufsS93Y52-D52cNMAaxKOH" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
--- 041_DBFace/11_tfjs/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1q2WP_TPoHAGVOifsFMS2kPBxiTWCsnN5" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1q2WP_TPoHAGVOifsFMS2kPBxiTWCsnN5" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
