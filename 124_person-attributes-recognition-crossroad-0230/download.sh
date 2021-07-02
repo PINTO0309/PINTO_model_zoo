87670c12841409ae237919ad31af319ca23ffd3e
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-07-02 23:34:18 +0900

    url updated

--- 124_person-attributes-recognition-crossroad-0230/download.sh
+++ 124_person-attributes-recognition-crossroad-0230/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1NgeCUEMkmkSUWbImEarMM-9YwYZwqAge" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1StJS7Ya7p-ypUCyTtJJEYBywdDrQNX0e" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1NgeCUEMkmkSUWbImEarMM-9YwYZwqAge" -o resources.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1StJS7Ya7p-ypUCyTtJJEYBywdDrQNX0e" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
