ad1ade906840b9c41f731b0582b6c25e84e3e746
pinto0309 <rmsdh122@yahoo.co.jp>
2021-09-03 17:39:31 +0900

    remove

--- 128_DeepCalib/demo/README.md
+++
@@ -1,4 +0,0 @@
-# Demo projects
-
-## DeepCalib with TensorFlow Lite in C++
-https://github.com/iwatake2222/play_with_tflite/tree/master/pj_tflite_camera_deep_calib
--- 128_DeepCalib/download_classification.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1ZkYaOsLBjGSSZpEadVc16acj2hzeaTu_" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1ZkYaOsLBjGSSZpEadVc16acj2hzeaTu_" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
