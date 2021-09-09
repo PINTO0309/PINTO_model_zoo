a8573d445553572348cdbf3b78342b3a7632bde6
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2021-09-10 07:05:17 +0900

    Update download.sh

--- 144_YuNet/download.sh
+++ 144_YuNet/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1apu4Xgx1mvbevcZfTChuEbmCqhkQCvPd" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1GJGDls66utI1WNG_WG5OdWaZWwfl_OT7" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1apu4Xgx1mvbevcZfTChuEbmCqhkQCvPd" -o resources.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1GJGDls66utI1WNG_WG5OdWaZWwfl_OT7" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
