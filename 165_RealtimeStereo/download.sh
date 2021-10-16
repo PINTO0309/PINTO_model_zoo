5adc88739e342f801a8c2f1a70223da555849d91
pinto0309 <rmsdh122@yahoo.co.jp>
2021-10-17 01:58:15 +0900

    Optimize and recommit the model. Added Myriad Blob.

--- 165_RealtimeStereo/download.sh
+++ 165_RealtimeStereo/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1Wsa0eUfjyYtWDZBEfZ2Z2RkPKnEeCgVq" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1To3O41da0adm0EKObJ8ig0FvC_ZETcRv" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1Wsa0eUfjyYtWDZBEfZ2Z2RkPKnEeCgVq" -o resources.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1To3O41da0adm0EKObJ8ig0FvC_ZETcRv" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
