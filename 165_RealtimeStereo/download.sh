9c757b08da463eb0306987a065caf884ab697f37
pinto0309 <rmsdh122@yahoo.co.jp>
2021-10-17 22:43:17 +0900

    comp

--- 165_RealtimeStereo/download.sh
+++ 165_RealtimeStereo/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1To3O41da0adm0EKObJ8ig0FvC_ZETcRv" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1pBH6ggIkaAnxAAhqfY5eMn8gHfBfRl82" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1To3O41da0adm0EKObJ8ig0FvC_ZETcRv" -o resources.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1pBH6ggIkaAnxAAhqfY5eMn8gHfBfRl82" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
