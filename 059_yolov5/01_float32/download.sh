715cc28ebfdf6df42ab27ba9959dacf0e9565943
pinto0309 <rmsdh122@yahoo.co.jp>
2021-11-05 09:40:23 +0900

    cleanup

--- 059_yolov5/01_float32/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=11OA1mSBWipUNJWSTCUlebtX32a1FD-rW" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=11OA1mSBWipUNJWSTCUlebtX32a1FD-rW" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
--- 059_yolov5/07_openvino/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1bOx-mCR4kAWdd1aVguy4rsBP5n2y5iFJ" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1bOx-mCR4kAWdd1aVguy4rsBP5n2y5iFJ" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
