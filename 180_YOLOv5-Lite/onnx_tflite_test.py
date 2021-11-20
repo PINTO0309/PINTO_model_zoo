9084ef2e98dbf0911d8473445933aaa0b8419506
pinto0309 <rmsdh122@yahoo.co.jp>
2021-11-20 14:48:21 +0900

    YOLOv5-Lite S

---
+++ 180_YOLOv5-Lite/onnx_tflite_test.py
@@ -0,0 +1,53 @@
+import onnxruntime
+import tensorflow as tf
+import time
+import numpy as np
+from pprint import pprint
+
+H=640
+W=640
+MODEL='model_float32'
+
+############################################################
+
+onnx_session = onnxruntime.InferenceSession(f'saved_model_640x640/model_float32.onnx')
+input_name = onnx_session.get_inputs()[0].name
+output_name = onnx_session.get_outputs()[0].name
+
+roop = 1
+e = 0.0
+result = None
+inp = np.ones((1,3,H,W), dtype=np.float32)
+for _ in range(roop):
+    s = time.time()
+    result = onnx_session.run(
+        [output_name],
+        {input_name: inp}
+    )
+    e += (time.time() - s)
+print('ONNX output @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+print(f'elapsed time: {e/roop*1000}ms')
+print(f'shape: {result[0].shape}')
+pprint(result)
+
+############################################################
+
+interpreter = tf.lite.Interpreter(model_path=f'saved_model_640x640/model_float32.tflite', num_threads=4)
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+roop = 1
+e = 0.0
+result = None
+inp = np.ones((1,H,W,3), dtype=np.float32)
+for _ in range(roop):
+    s = time.time()
+    interpreter.set_tensor(input_details[0]['index'], inp)
+    interpreter.invoke()
+    result = interpreter.get_tensor(output_details[0]['index'])
+    e += (time.time() - s)
+print('tflite output @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+print(f'elapsed time: {e/roop*1000}ms')
+print(f'shape: {result.shape}')
+pprint(result)
