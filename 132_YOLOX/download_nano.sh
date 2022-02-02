6316164462e73792bd9b17324e992aa03c8f1f86
pinto0309 <rmsdh122@yahoo.co.jp>
2022-02-02 17:38:30 +0900

    old model delete

--- 132_YOLOX/download_nano.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=12znoNNSYLgv-y7AElc2PIt81WFtRZ4d9" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=12znoNNSYLgv-y7AElc2PIt81WFtRZ4d9" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
