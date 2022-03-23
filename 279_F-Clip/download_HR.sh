e3cbbb55bdf8512e3e9cfa140299d60e744b2187
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-24 02:08:33 +0900

    HR updated

--- 279_F-Clip/download_HR.sh
+++ 279_F-Clip/download_HR.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1i_KwxPDphF3Ld0kxBk_bdSZLfJTl1DVy" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1BCfQocP2jCUthK8cHpSlIAvvRrMAKxbq" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1i_KwxPDphF3Ld0kxBk_bdSZLfJTl1DVy" -o resources.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1BCfQocP2jCUthK8cHpSlIAvvRrMAKxbq" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
