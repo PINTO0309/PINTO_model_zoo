afdba00417d89e4692389e352124d7f9dbdab693
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 10:57:56 +0900

    Support for Google Drive specification changes

--- 277_EDN-GTM/download_ohaze.sh
+++ 277_EDN-GTM/download_ohaze.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1LK9FPt0e-yLlUob6pIcNJ_Hux8UbMfjR" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1LK9FPt0e-yLlUob6pIcNJ_Hux8UbMfjR" -o resources.tar.gz
+fileid="1LK9FPt0e-yLlUob6pIcNJ_Hux8UbMfjR"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
