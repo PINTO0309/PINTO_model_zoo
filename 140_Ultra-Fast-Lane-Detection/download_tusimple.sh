7492f2ab5f6f844a5010dae0d9ce36adb11e37e8
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 17:25:47 +0900

    Support for Google Drive specification changes

--- 140_Ultra-Fast-Lane-Detection/download_tusimple.sh
+++ 140_Ultra-Fast-Lane-Detection/download_tusimple.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1Q-bGY7vkHKGcdCrIDooSx0MBUmrO8Iit" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1Q-bGY7vkHKGcdCrIDooSx0MBUmrO8Iit" -o resources.tar.gz
+fileid="1Q-bGY7vkHKGcdCrIDooSx0MBUmrO8Iit"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
