782c196fadf909f45488f505ec40fe13587481ec
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 12:57:30 +0900

    Support for Google Drive specification changes

--- 197_yolact-resnet50-fpn/download.sh
+++ 197_yolact-resnet50-fpn/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1WyWa9mrPV_0iV0PVRBc9tNhb8YLfQK24" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1WyWa9mrPV_0iV0PVRBc9tNhb8YLfQK24" -o resources.tar.gz
+fileid="1WyWa9mrPV_0iV0PVRBc9tNhb8YLfQK24"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
