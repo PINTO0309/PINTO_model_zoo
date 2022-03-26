8527eab0575264092f36ce302e82e07de27955f8
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 11:58:14 +0900

    Support for Google Drive specification changes

--- 225_NTIRE-2021-Dehazing-Two-branch/download.sh
+++ 225_NTIRE-2021-Dehazing-Two-branch/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1gnqNpIMXwA3QrtC5_p-3yyhJUlLjTEDB" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1gnqNpIMXwA3QrtC5_p-3yyhJUlLjTEDB" -o resources.tar.gz
+fileid="1gnqNpIMXwA3QrtC5_p-3yyhJUlLjTEDB"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
