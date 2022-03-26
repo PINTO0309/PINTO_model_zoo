db4e3afceb9413b5500b219d78f2ae3840decd4d
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 21:16:52 +0900

    Support for Google Drive specification changes

--- 065_ThreeDPoseUnityBarracuda/05_float16_quantization/download.sh
+++ 065_ThreeDPoseUnityBarracuda/05_float16_quantization/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1ikHjTmrFMagu1v11Rp3iD8ye6_9W-LXh" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1ikHjTmrFMagu1v11Rp3iD8ye6_9W-LXh" -o resources.tar.gz
+fileid="1ikHjTmrFMagu1v11Rp3iD8ye6_9W-LXh"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
