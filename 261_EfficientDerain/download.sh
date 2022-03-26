ebbc3cf68ee63300002da46f8177ba0b33da4d84
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 11:17:36 +0900

    Support for Google Drive specification changes

--- 261_EfficientDerain/download.sh
+++ 261_EfficientDerain/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1jRYdc8LHk9ofrlCM7Q85P8cTROCs1qXF" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1jRYdc8LHk9ofrlCM7Q85P8cTROCs1qXF" -o resources.tar.gz
+fileid="1jRYdc8LHk9ofrlCM7Q85P8cTROCs1qXF"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 267_LIOT/download.sh
+++ 267_LIOT/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=18933w0L4AwT16fiLXoWXMUK98yyTqyEV" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=18933w0L4AwT16fiLXoWXMUK98yyTqyEV" -o resources.tar.gz
+fileid="18933w0L4AwT16fiLXoWXMUK98yyTqyEV"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
