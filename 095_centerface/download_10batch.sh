cc44f9a1714e7d7877eb0e967da44fca7fe19324
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 19:23:37 +0900

    Support for Google Drive specification changes

--- 095_centerface/download_10batch.sh
+++ 095_centerface/download_10batch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=12oE1zHvRBisLe15Y08KYmHkO9rXdaGQP" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=12oE1zHvRBisLe15Y08KYmHkO9rXdaGQP" -o resources.tar.gz
+fileid="12oE1zHvRBisLe15Y08KYmHkO9rXdaGQP"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 098_SPICE/download.sh
+++ 098_SPICE/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=17057IKk8aQ5zNH7ApO9FGGjQDMmXSnOq" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=17057IKk8aQ5zNH7ApO9FGGjQDMmXSnOq" -o resources.tar.gz
+fileid="17057IKk8aQ5zNH7ApO9FGGjQDMmXSnOq"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
