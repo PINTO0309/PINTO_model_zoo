b5004ecfffcea0daa144281f3f6037aeb8ce1777
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 18:57:56 +0900

    Support for Google Drive specification changes

--- 127_dino/download_deits16.sh
+++ 127_dino/download_deits16.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1igtzyToEZpVKSzw56snmLo-KlB7IpF3O" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1igtzyToEZpVKSzw56snmLo-KlB7IpF3O" -o resources.tar.gz
+fileid="1igtzyToEZpVKSzw56snmLo-KlB7IpF3O"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
