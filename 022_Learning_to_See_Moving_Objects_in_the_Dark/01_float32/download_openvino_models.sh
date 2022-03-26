74988523c89eab49c901c9494fbd5c46269edf01
Ar-Ray-code <ray255ar@gmail.com>
2022-03-27 00:37:36 +0900

    update 022

--- 022_Learning_to_See_Moving_Objects_in_the_Dark/01_float32/download_openvino_models.sh
+++ 022_Learning_to_See_Moving_Objects_in_the_Dark/01_float32/download_openvino_models.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1goszH1_DlW2qaor8mTMY-f55NgSwxTAS" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1goszH1_DlW2qaor8mTMY-f55NgSwxTAS" -o openvino_models.tar.gz
+
+fileid="1goszH1_DlW2qaor8mTMY-f55NgSwxTAS"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o openvino_models.tar.gz
 tar -zxvf openvino_models.tar.gz
 rm openvino_models.tar.gz
+rm cookie
+
 echo Download finished.
--- 022_Learning_to_See_Moving_Objects_in_the_Dark/download_sample_movies.sh
+++ 022_Learning_to_See_Moving_Objects_in_the_Dark/download_sample_movies.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1kxtTDg9H50Onkzgv-mvt8n5hv69lxziW" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1kxtTDg9H50Onkzgv-mvt8n5hv69lxziW" -o sample_movies.tar.gz
+
+fileid="1kxtTDg9H50Onkzgv-mvt8n5hv69lxziW"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o sample_movies.tar.gz
 tar -zxvf sample_movies.tar.gz
 rm sample_movies.tar.gz
+rm cookie
+
 echo Download finished.
