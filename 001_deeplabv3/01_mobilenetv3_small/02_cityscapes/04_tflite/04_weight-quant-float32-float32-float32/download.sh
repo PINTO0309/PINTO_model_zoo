d23e9c5b865edff32bf3749e3a97ae2b3c230a56
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2022-03-27 00:08:26 +0900

    Fix double download

--- 001_deeplabv3/01_mobilenetv3_small/02_cityscapes/04_tflite/04_weight-quant-float32-float32-float32/download.sh
+++ 001_deeplabv3/01_mobilenetv3_small/02_cityscapes/04_tflite/04_weight-quant-float32-float32-float32/download.sh
@@ -4,8 +4,8 @@ fileid="1JzYLv7M0KVdLsqwu5lvRFEpUQv68hryk"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o deeplab_mnv3_small_weight_quant_257.tflite
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1HaZMOrJixipNz7Ia23xZh8C65v007mNe" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1HaZMOrJixipNz7Ia23xZh8C65v007mNe" -o deeplab_mnv3_small_weight_quant_769.tflite
+fileid="1JzYLv7M0KVdLsqwu5lvRFEpUQv68hryk"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o deeplab_mnv3_small_weight_quant_769.tflite
 
 echo Download finished.
