c83b7e4699dac01c0619f559b5aa044fd7463dad
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 20:04:01 +0900

    Support for Google Drive specification changes

--- 068_Colorful_Image_Colorization/02_weight_quantization/download.sh
+++ 068_Colorful_Image_Colorization/02_weight_quantization/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1o3yr2rxdAnO3rIo0F0DZxbJk-dbPP72H" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1o3yr2rxdAnO3rIo0F0DZxbJk-dbPP72H" -o resources.tar.gz
+fileid="1o3yr2rxdAnO3rIo0F0DZxbJk-dbPP72H"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
