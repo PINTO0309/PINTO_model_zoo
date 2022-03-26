7ff34fc2c66bb54f68c989d4dde36950a6262691
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 11:49:43 +0900

    Support for Google Drive specification changes

--- 232_MIMO-UNet/download.sh
+++ 232_MIMO-UNet/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1DiS9IGHj3VqDR2lWa8L89i2kV5GLnMil" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1DiS9IGHj3VqDR2lWa8L89i2kV5GLnMil" -o resources.tar.gz
+fileid="1DiS9IGHj3VqDR2lWa8L89i2kV5GLnMil"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
