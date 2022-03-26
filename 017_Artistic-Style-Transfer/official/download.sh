14b570f821b11538959d4e297b8ef63e49040052
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2022-03-27 00:12:37 +0900

    Fix double download

--- 017_Artistic-Style-Transfer/official/download.sh
+++ 017_Artistic-Style-Transfer/official/download.sh
@@ -4,8 +4,8 @@ fileid="1X_YS6Mr3Lmo9sLfCbVHa-RaKlni9FxF7"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o style_predict_quantized_256.tflite
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1mGwTGI1bcuTe4fUFGBvwr9AMGN0HcOzn" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1mGwTGI1bcuTe4fUFGBvwr9AMGN0HcOzn" -o style_transfer_quantized_dynamic.tflite
+fileid="1mGwTGI1bcuTe4fUFGBvwr9AMGN0HcOzn"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o style_transfer_quantized_dynamic.tflite
 
 echo Download finished.
