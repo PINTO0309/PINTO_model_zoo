313ebbdac26db701faf6e64e99231517ee7bf39a
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 17:16:06 +0900

    Support for Google Drive specification changes

--- 156_MobileHumanPose/download_mobile_human_pose_working_well.sh
+++ 156_MobileHumanPose/download_mobile_human_pose_working_well.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1vvjUsP_41Nqhj8oqRw2OpWjIdz8t5fxG" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1vvjUsP_41Nqhj8oqRw2OpWjIdz8t5fxG" -o resources.tar.gz
+fileid="1vvjUsP_41Nqhj8oqRw2OpWjIdz8t5fxG"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
