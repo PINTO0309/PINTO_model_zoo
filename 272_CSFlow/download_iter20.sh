3cdc1d018d7381a5eb052d5e3eba332f5da9ddaa
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 11:09:18 +0900

    Support for Google Drive specification changes

--- 272_CSFlow/download_iter20.sh
+++ 272_CSFlow/download_iter20.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1mjYegy8kMVtLn7u2v4D_EVU77ciEqdcD" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1mjYegy8kMVtLn7u2v4D_EVU77ciEqdcD" -o resources.tar.gz
+fileid="1mjYegy8kMVtLn7u2v4D_EVU77ciEqdcD"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 274_DeepFillv2/download.sh
+++ 274_DeepFillv2/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1g74aHCsG755s-BGMIk8abkIPG9fnuP4o" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1g74aHCsG755s-BGMIk8abkIPG9fnuP4o" -o resources.tar.gz
+fileid="1g74aHCsG755s-BGMIk8abkIPG9fnuP4o"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
