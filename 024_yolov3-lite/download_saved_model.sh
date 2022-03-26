4d91274dae68a883c814a8842f9817399c53c6d2
Ar-Ray-code <ray255ar@gmail.com>
2022-03-27 00:34:22 +0900

    update 024

--- 024_yolov3-lite/download_saved_model.sh
+++ 024_yolov3-lite/download_saved_model.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1byobKK7kfHuKumnada7rDSD1l2x9d6xO" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1byobKK7kfHuKumnada7rDSD1l2x9d6xO" -o saved_model.tar.gz
+fileid="1byobKK7kfHuKumnada7rDSD1l2x9d6xO"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o saved_model.tar.gz
 tar -zxvf saved_model.tar.gz
 rm saved_model.tar.gz
+rm cookie
+
 echo Download finished.
