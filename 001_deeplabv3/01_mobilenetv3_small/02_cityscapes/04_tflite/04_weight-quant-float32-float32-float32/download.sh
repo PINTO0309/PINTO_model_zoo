e4a013295f422045623436088ff27c3eedfb4abc
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2022-03-27 00:10:22 +0900

    Fix wrong id

--- 001_deeplabv3/01_mobilenetv3_small/02_cityscapes/04_tflite/04_weight-quant-float32-float32-float32/download.sh
+++ 001_deeplabv3/01_mobilenetv3_small/02_cityscapes/04_tflite/04_weight-quant-float32-float32-float32/download.sh
@@ -4,7 +4,7 @@ fileid="1JzYLv7M0KVdLsqwu5lvRFEpUQv68hryk"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o deeplab_mnv3_small_weight_quant_257.tflite
 
-fileid="1JzYLv7M0KVdLsqwu5lvRFEpUQv68hryk"
+fileid="1HaZMOrJixipNz7Ia23xZh8C65v007mNe"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o deeplab_mnv3_small_weight_quant_769.tflite
 
