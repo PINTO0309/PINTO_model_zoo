34e5847193435c91ab8fda0ac1d4b0ae1add4fa2
Nobuo Tsukamoto <nextremer.nb.o@gmail.com>
2022-03-26 19:47:33 +0900

    Support for Google Drive specification changes

--- 001_deeplabv3/02_mobilenetv3_large/02_cityscapes/04_tflite/02_uint8-dequant_float32-float32/download.sh
+++ 001_deeplabv3/02_mobilenetv3_large/02_cityscapes/04_tflite/02_uint8-dequant_float32-float32/download.sh
@@ -3,7 +3,5 @@
 fileid="1m_Qy4qtU1zzzYmcJcaDYGyhqX1Q0N8cd"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o deeplab_mnv3_large_cityscapes_trainfine.tflite
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
 
 echo Download finished.
