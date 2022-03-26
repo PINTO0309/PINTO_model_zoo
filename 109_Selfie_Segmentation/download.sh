d72887df7547656cbc09ae778175e6f7504f182f
pinto0309 <rmsdh122@yahoo.co.jp>
2022-03-26 19:17:24 +0900

    Support for Google Drive specification changes

--- 109_Selfie_Segmentation/download.sh
+++ 109_Selfie_Segmentation/download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=14xJrbB9S7kGcq8dkeI39z6W2W9axDFIq" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=14xJrbB9S7kGcq8dkeI39z6W2W9axDFIq" -o resources.tar.gz
+fileid="14xJrbB9S7kGcq8dkeI39z6W2W9axDFIq"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
