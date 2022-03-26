d70f00f9cd24dbe2924a6e2532e45d54187a0320
Ar-Ray-code <ray255ar@gmail.com>
2022-03-26 23:13:40 +0900

    update 020

--- 020_edgetpu-deeplab/01_float32/download_checkpoint_pb.sh
+++ 020_edgetpu-deeplab/01_float32/download_checkpoint_pb.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=11vCAygH0YBTS42E5N4l7RxsguyN08CkH" > /dev/null
-CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=11vCAygH0YBTS42E5N4l7RxsguyN08CkH" -o checkpoint_pb.tar.gz
+# 404 Not found ?
+fileid="11vCAygH0YBTS42E5N4l7RxsguyN08CkH"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o checkpoint_pb.tar.gz
 tar -zxvf checkpoint_pb.tar.gz
 rm checkpoint_pb.tar.gz
+rm cookie
+
 echo Download finished.
