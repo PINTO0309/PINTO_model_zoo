f14e33822c52970144b1f3279d2daa6621a1f143
pinto0309 <rmsdh122@yahoo.co.jp>
2022-05-05 09:02:01 +0900

    SeAFusion

---
+++ 291_SeAFusion/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fileid="1d4-W2LUJy11yCFw2cFH40Ezzy1hRHY1e"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
