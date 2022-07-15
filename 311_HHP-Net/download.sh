16bbf561828e6683a479576ace6768ca5a32f4f1
pinto0309 <rmsdh122@yahoo.co.jp>
2022-07-15 09:45:15 +0900

    HHP-Net

---
+++ 311_HHP-Net/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fileid="1Qfie8D9EDC841CycPfX1NdLklPjm0aiB"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
