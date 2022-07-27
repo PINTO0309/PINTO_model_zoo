7cc4a13ad43554d6d1f578d2e8dc764066eb4e3e
pinto0309 <rmsdh122@yahoo.co.jp>
2022-07-27 09:23:29 +0900

    end-to-end, LLIE+EXP+DET(DETR)

--- 315_Illumination-Adaptive-Transformer/download_e2e_llie_exp_det_merged.sh
+++ 315_Illumination-Adaptive-Transformer/download_e2e_llie_exp_det_merged.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fileid="1zhNpSWbAogKrcSnVKjMiD278c07u97ZN"
+fileid="1_jfHtYtW6AdFMV5QiHMk7ebFPtLGc8dr"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
