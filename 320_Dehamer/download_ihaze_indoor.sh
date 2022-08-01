e8a684b3a90a2ab23f9007bb58b34a453d371969
pinto0309 <rmsdh122@yahoo.co.jp>
2022-08-01 10:01:11 +0900

    ihaze + indoor

--- 320_Dehamer/download_ihaze_indoor.sh
+++ 320_Dehamer/download_ihaze_indoor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fileid="1P9_60djynind9ssWB8dEi34RcdgxTjwl"
+fileid="15mEuKKSDDbAoDttHXyKHt0e6DVqQc8sO"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
