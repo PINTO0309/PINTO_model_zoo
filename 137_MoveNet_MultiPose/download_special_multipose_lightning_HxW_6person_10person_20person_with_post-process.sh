e9ed6c6b3c4fbc26b4ff0f262559458c461aabf0
pinto0309 <rmsdh122@yahoo.co.jp>
2022-09-15 14:49:52 +0900

    20 persons

--- 137_MoveNet_MultiPose/download_special_multipose_lightning_HxW_6person_10person_20person_with_post-process.sh
+++ 137_MoveNet_MultiPose/download_special_multipose_lightning_HxW_6person_10person_20person_with_post-process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fileid="1XHYiwx97GXZxyOGRC36kk5dKfQv4lgPm"
+fileid="1WVaurt_7DTwSVhSspHZxH5xO-qb0yMpJ"
 html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
 tar -zxvf resources.tar.gz
