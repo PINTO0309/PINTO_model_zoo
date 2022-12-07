3e3db47ef9674232aaf195efecfad83c075115fd
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-07 16:54:26 +0900

    E2Pose with PostProcess

---
+++ 333_E2Pose/download_with_postprocess.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fileid="1o8bMPziS5rnNiV_CFl1uG-5RDSHT54eJ"
+html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
+curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
