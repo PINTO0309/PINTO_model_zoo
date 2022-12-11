af255925611970af78bd10286ba9a4ab7abdd2ae
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-11 17:37:23 +0900

    Google Drive to Wasabi Storage

--- 333_E2Pose/download_with_postprocess.sh
+++ 333_E2Pose/download_with_postprocess.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-fileid="1o8bMPziS5rnNiV_CFl1uG-5RDSHT54eJ"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/333_E2Pose/resources_post.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
 echo Download finished.
+https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/333_E2Pose/resources_post.tar.gz
