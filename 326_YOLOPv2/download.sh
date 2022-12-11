10c04f7599742aa0b4a32e9397ed03ee8ba3c9d2
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-11 19:24:38 +0900

    Google Drive to Wasabi Storage

--- 326_YOLOPv2/download.sh
+++ 326_YOLOPv2/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="14ZM0R1A3jywA0MgxqdHy4pvFAz__bU1t"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/326_YOLOPv2/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
