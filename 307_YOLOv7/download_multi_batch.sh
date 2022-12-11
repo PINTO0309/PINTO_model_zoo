17bc091539b92fe9adb971c12fa7272ec1756582
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-11 23:50:31 +0900

    Google Drive to Wasabi Storage

--- 307_YOLOv7/download_multi_batch.sh
+++ 307_YOLOv7/download_multi_batch.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1PrV-9oY1n5ptCF-YRymwp4mHOadK5DZK"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/307_YOLOv7/no-postprocess/resources_n.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 307_YOLOv7/download_openvino_myriad_oakd_tf_tflite.sh
+++ 307_YOLOv7/download_openvino_myriad_oakd_tf_tflite.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1TmtLbJdcKftUZ9ybVOlSkKQr-pCD6bYL"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/307_YOLOv7/openvino_myriad_tf/resources_openvino_myriad_tf.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
