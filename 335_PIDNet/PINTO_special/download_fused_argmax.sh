7e7814ba0a62b833d85541ee37188bfef23f7ad4
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-11 16:49:42 +0900

    Google Drive to Wasabi Storage

--- 335_PIDNet/PINTO_special/download_fused_argmax.sh
+++ 335_PIDNet/PINTO_special/download_fused_argmax.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1FRk1ABLJMOK2bJYN_OcspC_WSdocN0Bf"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/335_PIDNet/resources_fused_argmax.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
