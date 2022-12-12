5d8744721de5d4607ec3c8fd34d510c5d871e878
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-12 09:57:15 +0900

    Google Drive to Wasabi Storage

--- 299_DGNet/download.sh
+++ 299_DGNet/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1K4FO3egxcIkUPJkbv6ocOAZbsP-qz7QE"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/299_DGNet/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
