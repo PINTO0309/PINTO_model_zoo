234886396cf9236c3ace7f5c752808add1b35a62
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-12 23:39:08 +0900

    Google Drive to Wasabi Storage

--- 288_perceptual-reflection-removal/download.sh
+++ 288_perceptual-reflection-removal/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1EtL3rv8sjOZz3O_l1xuW1ahVI-tvt8ZG"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/288_perceptual-reflection-removal/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
