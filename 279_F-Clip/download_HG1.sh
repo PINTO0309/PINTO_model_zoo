f45bea25d83178ae524b5bb7ecacb6436b468e90
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-13 00:31:14 +0900

    Google Drive to Wasabi Storage

--- 279_F-Clip/download_HG1.sh
+++ 279_F-Clip/download_HG1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1ZPPMocYoxGylT5StsmaWPoJ19Csmb_W0"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/279_F-Clip/HG1.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
