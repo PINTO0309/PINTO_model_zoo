42babe81a34a81078218c0a8dace720bb47a03a2
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-12 10:35:15 +0900

    Google Drive to Wasabi Storage

--- 290_AdaFace/download.sh
+++ 290_AdaFace/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1_G3nTy_N6vQUv7qOyPnIX_NAEEI2MTgO"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/290_AdaFace/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 291_SeAFusion/download.sh
+++ 291_SeAFusion/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1d4-W2LUJy11yCFw2cFH40Ezzy1hRHY1e"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/291_SeAFusion/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
