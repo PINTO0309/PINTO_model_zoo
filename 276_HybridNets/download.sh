5ff9187f57d4c6e72ae7a08eba5511a96e934d14
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-13 10:50:45 +0900

    Google Drive to Wasabi Storage

--- 276_HybridNets/download.sh
+++ 276_HybridNets/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1r1jDtJhi-5KfyMOee0CEbAC-N_hmf8t7"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/276_HybridNets/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
