9940015bbeae39970d29366b432d21807c38c5f9
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-13 23:52:23 +0900

    Google Drive to Wasabi Storage

--- 263_EgoNet/download.sh
+++ 263_EgoNet/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1B1730O3yTzFiqUa31adDgG6QxbF0BS8Q"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/263_EgoNet/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
