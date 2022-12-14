6b3e3811e08e7965e03160391d0847134e06ca48
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-14 22:01:56 +0900

    Google Drive to Wasabi Storage

--- 259_Emotion_FERPlus/download.sh
+++ 259_Emotion_FERPlus/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1Rx79CJISYhuDBHaX7A2yHtjJEbL8waog"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/259_Emotion_FERPlus/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
