3b51db72c5ce2c4b1b6fbdf798c4a95a8581b9ea
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-14 21:54:42 +0900

    Google Drive to Wasabi Storage

--- 262_ByteTrack/download.sh
+++ 262_ByteTrack/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1j5Cl1miScI5d6vk4ndNHzx5oCxL6gabq"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/262_ByteTrack/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
