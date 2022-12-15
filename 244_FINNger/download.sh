95d102276d4effd330aa3f667bb6671dc0339738
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-15 11:11:23 +0900

    Google Drive to Wasabi Storage

--- 244_FINNger/download.sh
+++ 244_FINNger/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="18_jqEgz4Guh8rZwXGn8cBe-VmEx7aysi"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/244_FINNger/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
