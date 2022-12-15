f2c54cbc5c5ed54e3eb7a6a2ef83ddf05a27fe54
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-15 10:07:24 +0900

    Google Drive to Wasabi Storage

--- 250_Face-Mask-Detection/download_maskdetector.sh
+++ 250_Face-Mask-Detection/download_maskdetector.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1Y_8S_2P9meYFg1Vw2_eWlVUFd5sXNMlv"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/250_Face-Mask-Detection/maskdetector.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
