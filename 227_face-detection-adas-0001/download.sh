f9ed55a0dfdfff0aec7c3e0d4ca1208307c6ff1e
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-18 01:01:44 +0900

    Google Drive to Wasabi Storage

--- 227_face-detection-adas-0001/download.sh
+++ 227_face-detection-adas-0001/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1sWwmmjPKATKZ2nCq7PhF1lprYHkxSqef"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/227_face-detection-adas-0001/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
