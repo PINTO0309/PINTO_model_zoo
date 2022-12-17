ee32b5d4871aa56967e4bba3bdf9bf24ff46fa13
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-17 23:38:59 +0900

    Google Drive to Wasabi Storage

--- 240_BSRGAN/download_x4.sh
+++ 240_BSRGAN/download_x4.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1gFcJp8hCDQ0np7sxi2HuZB4GvwkMUCpw"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/240_BSRGAN/resources_x4.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 242_RobustVideoMatting/download_resnet50.sh
+++ 242_RobustVideoMatting/download_resnet50.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1S7sRjjsLfg6bHU8MtnPinCiuN6z5iWQN"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/242_RobustVideoMatting/resources_resnet50.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
