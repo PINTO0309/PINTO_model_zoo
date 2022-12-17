35456f58cf8b97e78cd1b2a11283b3bac733a843
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-18 00:20:09 +0900

    Google Drive to Wasabi Storage

--- 236_A-TVSNet/download.sh
+++ 236_A-TVSNet/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1kTn-IyQ36DgrukQ_HMdmHkh6LGSMU7fC"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/236_A-TVSNet/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
