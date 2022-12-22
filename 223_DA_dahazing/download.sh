68a442bdd592f69f8c0e5f4267a436cfcaac4432
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-22 10:54:44 +0900

    Google Drive to Wasabi Storage

--- 223_DA_dahazing/download.sh
+++ 223_DA_dahazing/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1s9ljl1XOxO3UI0KEkhCKNagRTziJUAG4"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/223_DA_dahazing/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
