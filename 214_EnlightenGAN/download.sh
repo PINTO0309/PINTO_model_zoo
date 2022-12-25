54f6ecb420f0e092b56c527a497e031d3f0de39c
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-25 17:00:05 +0900

    Google Drive to Wasabi Storage

--- 214_EnlightenGAN/download.sh
+++ 214_EnlightenGAN/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1hff09xCNq5BawjC6ju-hyemFvKiigu8s"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/214_EnlightenGAN/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
