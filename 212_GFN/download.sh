b0a9ebab30a8600462c7a5541d12a51be2e658a6
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-25 17:07:35 +0900

    Google Drive to Wasabi Storage

--- 212_GFN/download.sh
+++ 212_GFN/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1V6MbSrcpVR6h7VxBWbKiyEQx0vNogQAv"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/212_GFN/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
