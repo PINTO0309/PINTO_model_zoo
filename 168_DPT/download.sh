3ff4f1ed9ec70d23f2e3f6e0604336124272c49e
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-30 00:04:34 +0900

    Google Drive to Wasabi Storage

--- 168_DPT/download.sh
+++ 168_DPT/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1uUN-qizwVKSCkb4bL7RO3qsN1Eba7til"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/168_DPT/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 169_spaghettinet_edgetpu/download.sh
+++ 169_spaghettinet_edgetpu/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1SQv18VG2OW9k9ZCx5_suPOBoUQCz_2v_"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/169_spaghettinet_edgetpu/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
