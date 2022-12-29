74eea80b2d11910b3ebec65fbbc5a348d13f56ce
pinto0309 <rmsdh122@yahoo.co.jp>
2022-12-29 22:40:20 +0900

    Google Drive to Wasabi Storage

--- 185_person-vehicle-bike-detection-crossroad-0078/download.sh
+++ 185_person-vehicle-bike-detection-crossroad-0078/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="12mUKory2Q_Mp1qYVB0LRMaCRwzTBKKO-"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/185_person-vehicle-bike-detection-crossroad-0078/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 189_vehicle-license-plate-detection-barrier-0106/download.sh
+++ 189_vehicle-license-plate-detection-barrier-0106/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1wrGSH74UoJJyd6wzrxknY1rnmguYnCDQ"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/189_vehicle-license-plate-detection-barrier-0106/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
