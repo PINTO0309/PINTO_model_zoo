102d5080367367ff91c0cf8e0a197ee2b9ef2fbd
pinto0309 <rmsdh122@yahoo.co.jp>
2023-01-06 11:23:38 +0900

    Google Drive to Wasabi Storage

--- 157_3DMPPE_POSENET/download_192x192.sh
+++ 157_3DMPPE_POSENET/download_192x192.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1LnSXCqpM0t3ZOvIZwJ9gwWXJa5QbRrup"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/157_3DMPPE_POSENET/saved_model_192x192.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 157_3DMPPE_POSENET/download_256x256.sh
+++ 157_3DMPPE_POSENET/download_256x256.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1PK1GzNVu9slqee_yHJ3v0hV_n3Zc8Lfi"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/157_3DMPPE_POSENET/saved_model_256x256.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
