5067d253935ca7349ed692616a019016e739edf5
pinto0309 <rmsdh122@yahoo.co.jp>
2023-01-06 11:36:57 +0900

    Google Drive to Wasabi Storage

--- 154_driver-action-recognition-adas-0002-encoder/download.sh
+++ 154_driver-action-recognition-adas-0002-encoder/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1gb-aDZ5bmBIxtuQSpr4FNl9qYKWB5itp"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/154_driver-action-recognition-adas-0002-encoder/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
