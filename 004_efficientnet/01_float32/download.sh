6523e214e078ef9fa6d44d258ee076e6e9585b44
pinto0309 <rmsdh122@yahoo.co.jp>
2023-02-23 14:16:22 +0900

    Google Drive to Wasabi Storage

--- 004_efficientnet/01_float32/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1d4G1DsoEAeam9RD7597bDt7Ex-7eIeiK"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
--- 004_efficientnet/03_integer_quantization/download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-fileid="1vRWYDARVyRXNgCq5dzsGD9Wb3TgB_up2"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o efficientnet_b0_224_integer_quant.tflite
-
-echo Download finished.
