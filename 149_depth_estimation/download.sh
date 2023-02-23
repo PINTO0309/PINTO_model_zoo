70bbbb6bce7f058204721e6921baf477b76621ca
pinto0309 <rmsdh122@yahoo.co.jp>
2023-02-23 13:50:16 +0900

    Google Drive to Wasabi Storage

--- 149_depth_estimation/download.sh
+++ 149_depth_estimation/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1A_Wr88UcH4R7C_N31d6k9U_gxkvJfV28"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/149_depth_estimation/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
