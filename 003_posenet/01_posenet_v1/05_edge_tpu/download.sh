bd6d3a0dec3ca03af5d92e9ca621499b9a8d9708
pinto0309 <rmsdh122@yahoo.co.jp>
2023-02-23 14:12:41 +0900

    Google Drive to Wasabi Storage

--- 003_posenet/01_posenet_v1/05_edge_tpu/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1X5aoKDTW0h5dg2BD3u4ciaA2DXrmTNI1"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
--- 003_posenet/02_posenet_v2/05_full_integer_quantization/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1hSRKFQG_fEuZzgtni15O98yr_H5xgO_S"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
