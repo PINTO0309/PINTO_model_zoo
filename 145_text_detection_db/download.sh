d2bd6ff715f7b3b9afcc6d3141bce2dd6e701597
pinto0309 <rmsdh122@yahoo.co.jp>
2023-03-15 00:16:17 +0900

    Google Drive to Wasabi Storage

--- 145_text_detection_db/download.sh
+++ 145_text_detection_db/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1zt8ZcMIRkRMuN2m_voC6R9lwaL5HyKa1"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/145_text_detection_db/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
