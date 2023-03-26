150856f81fd9f0db42b0f0e98f1fad4bcdfc5fcc
pinto0309 <rmsdh122@yahoo.co.jp>
2023-03-26 22:22:12 +0900

    Google Drive to Wasabi Storage

--- 126_person-attributes-recognition-crossroad-0238/download.sh
+++ 126_person-attributes-recognition-crossroad-0238/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1dorcMgmXVOXCvMNL7nkNTxp1GQKWDr-I"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/126_person-attributes-recognition-crossroad-0238/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
