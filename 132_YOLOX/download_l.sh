a6edc8ddd18734fa6c32b9d23971c0b3ea63e34c
pinto0309 <rmsdh122@yahoo.co.jp>
2023-03-26 21:57:48 +0900

    Google Drive to Wasabi Storage

--- 132_YOLOX/download_l.sh
+++ 132_YOLOX/download_l.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1LJvOlzkDghkV8xGdlLk0UZ-WFWSjgf4I"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/132_YOLOX/resources_l.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
