af0aabc04c66093a12a07b0f7f3f6527cf3ba813
pinto0309 <rmsdh122@yahoo.co.jp>
2023-04-21 10:26:35 +0900

    demo

---
+++ 374_LaneSOD/demo/README.md
@@ -0,0 +1,16 @@
+# Demo projects
+
+## LaneSOD with ONNX Runtime in Python
+
+- movie
+
+  https://s3.ap-northeast-2.wasabisys.com/test-movie/352x576_dehaze.mp4
+
+- script
+  ```bash
+  python demo_LaneSOD_onnx.py
+  ```
+
+- result
+
+
---
+++ 374_LaneSOD/demo/demo_LaneSOD_onnx.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+
+import os
+import copy
+import time
+import argparse
+from typing import List
+import cv2 as cv
+import numpy as np
+import onnxruntime
+
+MEAN = np.asarray([[[[0.485]],[[0.456]],[[0.406]]]], dtype=np.float32)
+STD = np.asarray([[[[0.229]],[[0.224]],[[0.225]]]], dtype=np.float32)
+
+def run_inference(
+    onnx_session: onnxruntime.InferenceSession,
+    input_name: str,
+    input_shape: List[int],
+    threshold: float,
+    image: np.ndarray,
+) -> np.ndarray:
+    # Pre process:Resize, BGR->RGB, Transpose, float32 cast
+    input_image = cv.resize(
+        src=image,
+        dsize=(input_shape[3], input_shape[2]),
+    )
+    input_image = cv.cvtColor(
+        src=input_image,
+        code=cv.COLOR_BGR2RGB,
+    )
+    input_image = input_image.transpose(2, 0, 1)
+    input_image = np.expand_dims(input_image, axis=0)
+    input_image = input_image.astype('float32')
+    input_image = input_image / 255.0
+    input_image = (input_image - MEAN) / STD
+
+    # Inference
+    result = onnx_session.run(
+        output_names=None,
+        input_feed={
+            input_name: input_image,
+        },
+    )
+
+    # Post process: Transpose, Resize, uint8 cast
+    mask = result[0][0].transpose(1,2,0)
+    mask = np.concatenate([mask, mask, mask], axis=2)
+    mask = cv.resize(
+        src=mask,
+        dsize=(image.shape[1], image.shape[0]),
+    )
+    mask = np.where(mask >= threshold, 0, 1).astype(np.uint8)
+    output_image = image * mask
+    output_image[..., 1] = \
+        np.where(
+            output_image[..., 1] == 0, 255, output_image[..., 1]
+        ).astype(np.uint8)
+
+    return output_image
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-d',
+        '--device',
+        type=int,
+        default=0,
+    )
+    parser.add_argument(
+        '-f',
+        '--movie_file',
+        type=str,
+        default='352x576_dehaze.mp4',
+    )
+    parser.add_argument(
+        '-m',
+        '--model',
+        type=str,
+        default='./lanesod_352x576.onnx',
+    )
+    parser.add_argument(
+        '-t',
+        '--threshold',
+        type=int,
+        default=0.05,
+        help='Segmentation area thresholds.'
+    )
+
+
+    args = parser.parse_args()
+    model_path = args.model
+    threshold = args.threshold
+
+    # Initialize video capture
+    cap_device = args.device
+    if args.movie_file is not None:
+        cap_device = args.movie_file
+    cap = cv.VideoCapture(cap_device)
+    cap_fps = cap.get(cv.CAP_PROP_FPS)
+    w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    fourcc = cv.VideoWriter_fourcc('m', 'p', '4', 'v')
+    video_writer = cv.VideoWriter(
+        filename='output.mp4',
+        fourcc=fourcc,
+        fps=cap_fps,
+        frameSize=(w, h*2),
+    )
+    WINDOW_NAME = 'LaneSOD test'
+    cv.namedWindow(WINDOW_NAME, cv.WINDOW_AUTOSIZE)
+
+    # Load model
+    model_dir = os.path.dirname(model_path)
+    if model_dir == '':
+        model_dir = '.'
+
+    onnx_session = onnxruntime.InferenceSession(
+        model_path,
+        providers=[
+            (
+                'TensorrtExecutionProvider', {
+                    'trt_engine_cache_enable': True,
+                    'trt_engine_cache_path': model_dir,
+                    'trt_fp16_enable': True,
+                }
+            ),
+            'CUDAExecutionProvider',
+            'CPUExecutionProvider'
+        ],
+    )
+
+    model_inputs = onnx_session.get_inputs()
+    input_name = model_inputs[0].name
+    input_shape = model_inputs[0].shape
+
+    while True:
+        # Capture read
+        ret, frame = cap.read()
+        if not ret:
+            break
+        debug_image = copy.deepcopy(frame)
+
+        # Inference execution
+        start_time = time.time()
+        output_image = run_inference(
+            onnx_session=onnx_session,
+            input_name=input_name,
+            input_shape=input_shape,
+            threshold=threshold,
+            image=frame,
+        )
+        elapsed_time = time.time() - start_time
+
+        output_image = cv.resize(
+            output_image,
+            dsize=(
+                debug_image.shape[1],
+                debug_image.shape[0],
+            )
+        )
+
+        # Inference elapsed time
+        cv.putText(
+            output_image,
+            f"Elapsed Time : {elapsed_time * 1000:.1f} ms",
+            (10, 35),
+            cv.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (255, 255, 255),
+            2,
+            cv.LINE_AA,
+        )
+        cv.putText(
+            output_image,
+            f"Elapsed Time : {elapsed_time * 1000:.1f} ms",
+            (10, 35),
+            cv.FONT_HERSHEY_SIMPLEX,
+            0.7,
+            (0, 255, 0),
+            1,
+            cv.LINE_AA,
+        )
+
+        key = cv.waitKey(1)
+        if key == 27:  # ESC
+            break
+
+        combined_img = np.vstack([debug_image, output_image])
+        cv.imshow(WINDOW_NAME, combined_img)
+        video_writer.write(combined_img)
+
+    if video_writer is not None:
+        video_writer.release()
+    if cap is not None:
+        cap.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
