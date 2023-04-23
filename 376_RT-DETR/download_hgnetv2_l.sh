a1df481282e23d5a9f0b3ba3f9bf5e3cd5c55ba2
pinto0309 <rmsdh122@yahoo.co.jp>
2023-04-23 16:57:48 +0900

    HGNetv2_L

---
+++ 376_RT-DETR/download_hgnetv2_l.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/376_RT-DETR/resources_hgnetv2_l.tar.gz" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
