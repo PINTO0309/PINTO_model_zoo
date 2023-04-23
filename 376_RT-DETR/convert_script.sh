3936fffb6532ff79e9d8a64dbc0275a188bdc367
pinto0309 <rmsdh122@yahoo.co.jp>
2023-04-23 16:28:58 +0900

    update

--- 376_RT-DETR/convert_script.sh
+++ 376_RT-DETR/convert_script.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# https://zenn.dev/pinto0309/scraps/63167cc8709f0b
+# https://github.com/PaddlePaddle/PaddleDetection/tree/develop/configs/rtdetr
+
+# pip install onnx==1.13.1
+# pip install paddle2onnx==1.0.5
+# pip install onnxsim==0.4.17
+
+git clone https://github.com/PaddlePaddle/PaddleDetection.git
+cd PaddleDetection/
+# git checkout develop
+git checkout 72ae571da36aff55058c70f302e57097ad6278fc
+
+MODELS=(
+    "rtdetr_r50vd_6x_coco"
+    "rtdetr_r101vd_6x_coco"
+    "rtdetr_hgnetv2_l_6x_coco"
+    "rtdetr_hgnetv2_x_6x_coco"
+)
+
 RESOLUTIONS=(
     "192 320"
     "192 416"
@@ -40,57 +59,64 @@ RESOLUTIONS=(
 
 wget https://github.com/jiangjiajun/PaddleUtils/raw/main/paddle/paddle_infer_shape.py
 
-for((j=0; j<${#RESOLUTIONS[@]}; j++))
+for((i=0; i<${#MODELS[@]}; i++))
 do
-    RESOLUTION=(`echo ${RESOLUTIONS[j]}`)
-    H=${RESOLUTION[0]}
-    W=${RESOLUTION[1]}
-    echo @@@@@@@@@@@@@@@@@ processing ${H}x${W} ...
+    MODEL=(`echo ${MODELS[i]}`)
 
-    sed -i -e \
-    "s/\[640, 640\]/\[${H}, ${W}\]/g" \
-    configs/rtdetr/_base_/rtdetr_r50vd.yml
+    for((j=0; j<${#RESOLUTIONS[@]}; j++))
+    do
+        RESOLUTION=(`echo ${RESOLUTIONS[j]}`)
+        H=${RESOLUTION[0]}
+        W=${RESOLUTION[1]}
+        echo @@@@@@@@@@@@@@@@@ processing ${MODEL}_${H}x${W} ...
 
-    sed -i -e \
-    "s/\[3, 640, 640\]/\[3, ${H}, ${W}\]/g" \
-    configs/rtdetr/_base_/rtdetr_reader.yml
+        sed -i -e \
+        "s/\[640, 640\]/\[${H}, ${W}\]/g" \
+        configs/rtdetr/_base_/rtdetr_r50vd.yml
 
-    sed -i -e \
-    "s/\[640, 640\]/\[${H}, ${W}\]/g" \
-    configs/rtdetr/_base_/rtdetr_reader.yml
+        sed -i -e \
+        "s/\[3, 640, 640\]/\[3, ${H}, ${W}\]/g" \
+        configs/rtdetr/_base_/rtdetr_reader.yml
 
-    python tools/export_model.py \
-    -c configs/rtdetr/rtdetr_r50vd_6x_coco.yml \
-    -o weights=https://bj.bcebos.com/v1/paddledet/models/rtdetr_r50vd_6x_coco.pdparams trt=True use_gpu=False \
-    --output_dir=output_inference
+        sed -i -e \
+        "s/\[640, 640\]/\[${H}, ${W}\]/g" \
+        configs/rtdetr/_base_/rtdetr_reader.yml
 
-    python paddle_infer_shape.py \
-    --model_dir=output_inference/rtdetr_r50vd_6x_coco/ \
-    --model_filename model.pdmodel  \
-    --params_filename model.pdiparams \
-    --save_dir rtdetr_r50vd_6x_coco \
-    --input_shape_dict="{'image':[1, 3, ${H}, ${W}], 'im_shape': [1, 2], 'scale_factor': [1, 2]}"
+        python tools/export_model.py \
+        -c configs/rtdetr/${MODEL}.yml \
+        -o weights=https://bj.bcebos.com/v1/paddledet/models/${MODEL}.pdparams trt=True use_gpu=False \
+        --output_dir=output_inference
 
-    paddle2onnx \
-    --model_dir=rtdetr_r50vd_6x_coco/ \
-    --model_filename model.pdmodel  \
-    --params_filename model.pdiparams \
-    --opset_version 16 \
-    --save_file rtdetr_r50vd_6x_coco_${H}x${W}.onnx
+        python paddle_infer_shape.py \
+        --model_dir=output_inference/${MODEL}/ \
+        --model_filename model.pdmodel  \
+        --params_filename model.pdiparams \
+        --save_dir ${MODEL} \
+        --input_shape_dict="{'image':[1, 3, ${H}, ${W}], 'im_shape': [1, 2], 'scale_factor': [1, 2]}"
 
-    onnxsim rtdetr_r50vd_6x_coco_${H}x${W}.onnx rtdetr_r50vd_6x_coco_${H}x${W}.onnx
-    onnxsim rtdetr_r50vd_6x_coco_${H}x${W}.onnx rtdetr_r50vd_6x_coco_${H}x${W}.onnx
-    onnxsim rtdetr_r50vd_6x_coco_${H}x${W}.onnx rtdetr_r50vd_6x_coco_${H}x${W}.onnx
+        paddle2onnx \
+        --model_dir=${MODEL}/ \
+        --model_filename model.pdmodel  \
+        --params_filename model.pdiparams \
+        --opset_version 16 \
+        --save_file ${MODEL}_${H}x${W}.onnx
 
-    sed -i -e \
-    "s/\[${H}, ${W}\]/\[640, 640\]/g" \
-    configs/rtdetr/_base_/rtdetr_r50vd.yml
+        onnxsim ${MODEL}_${H}x${W}.onnx ${MODEL}_${H}x${W}.onnx
+        onnxsim ${MODEL}_${H}x${W}.onnx ${MODEL}_${H}x${W}.onnx
+        onnxsim ${MODEL}_${H}x${W}.onnx ${MODEL}_${H}x${W}.onnx
 
-    sed -i -e \
-    "s/\[3, ${H}, ${W}\]/\[3, 640, 640\]/g" \
-    configs/rtdetr/_base_/rtdetr_reader.yml
+        sed -i -e \
+        "s/\[${H}, ${W}\]/\[640, 640\]/g" \
+        configs/rtdetr/_base_/rtdetr_r50vd.yml
 
-    sed -i -e \
-    "s/\[${H}, ${W}\]/\[640, 640\]/g" \
-    configs/rtdetr/_base_/rtdetr_reader.yml
+        sed -i -e \
+        "s/\[3, ${H}, ${W}\]/\[3, 640, 640\]/g" \
+        configs/rtdetr/_base_/rtdetr_reader.yml
+
+        sed -i -e \
+        "s/\[${H}, ${W}\]/\[640, 640\]/g" \
+        configs/rtdetr/_base_/rtdetr_reader.yml
+    done
 done
+
+
