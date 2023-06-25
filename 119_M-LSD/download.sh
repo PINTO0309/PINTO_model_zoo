2fca5c55eb64592b1b43d538fce9c244b01c34c3
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-25 21:58:51 +0900

    Google Drive to Wasabi Storage

--- 119_M-LSD/download.sh
+++ 119_M-LSD/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1Gr8R44UDNcipR5ZY4qia71NLoNdqwa_K"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/119_M-LSD/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
