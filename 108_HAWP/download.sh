56675ec8afa2db68b8846babb2ba33a92fcb153d
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-25 22:48:24 +0900

    Google Drive to Wasabi Storage

--- 108_HAWP/download.sh
+++ 108_HAWP/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1l3HB86ZZm5n1Kn3W1QIwB0NGqdcrZXu9"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/108_HAWP/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
