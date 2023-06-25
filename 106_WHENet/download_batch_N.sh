93ba6d154ffbc489f718a579faff7e1784054fd9
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-25 22:52:32 +0900

    Google Drive to Wasabi Storage

--- 106_WHENet/download_batch_N.sh
+++ 106_WHENet/download_batch_N.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1HAnmoV9OcIcZlEPciYEJ8HkoFx8iBxm3"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/106_WHENet/resources_batch_N.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
