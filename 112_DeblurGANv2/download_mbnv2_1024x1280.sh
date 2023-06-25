6a34c34d59ba5fc8d0084184ed5dabd7de6a80c5
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-25 22:38:13 +0900

    Google Drive to Wasabi Storage

--- 112_DeblurGANv2/download_mbnv2_1024x1280.sh
+++ 112_DeblurGANv2/download_mbnv2_1024x1280.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1xiWHx3d-BswgSPMb4_dLwX8BtCBRCnrq"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/112_DeblurGANv2/saved_model_mbnv2_1024x1280.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 112_DeblurGANv2/download_mbnv2_736x1280.sh
+++ 112_DeblurGANv2/download_mbnv2_736x1280.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1hB9cmdm7MRN7QTp-inG0qgymlHqITGPn"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/112_DeblurGANv2/saved_model_mbnv2_736x1280.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
