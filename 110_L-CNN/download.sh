623285f707a748fef15b5d05865fe7e27a1b1cc9
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-25 22:44:09 +0900

    Google Drive to Wasabi Storage

--- 110_L-CNN/download.sh
+++ 110_L-CNN/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1ZvJjDnWSIFE82gD_JwLRAG0-SXj2r1vM"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/110_L-CNN/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
