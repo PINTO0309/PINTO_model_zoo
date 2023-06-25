1c5b89f186ded19457f4aaf16f0ccb189e072f71
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-25 22:00:28 +0900

    Google Drive to Wasabi Storage

--- 118_Speech-enhancement/download.sh
+++ 118_Speech-enhancement/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1jbTQKj0G2sW9-2pdwf8P3p8oZAcplagP"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/118_Speech-enhancement/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
