b80ecdc76340ac5810d63d2806fa717d5b5301a7
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-25 22:42:37 +0900

    Google Drive to Wasabi Storage

--- 111_SRN-Deblur/download_720x1280.sh
+++ 111_SRN-Deblur/download_720x1280.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1Zw8U0YdRXrc8gAMgC4bkPt8YHdIJu_S2"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/111_SRN-Deblur/resources_720x1280.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
