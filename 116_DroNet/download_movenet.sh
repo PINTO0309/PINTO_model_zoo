d99a4da53945f1f9f020fd15eaefe1dc455c4e54
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-25 22:05:51 +0900

    Google Drive to Wasabi Storage

--- 116_DroNet/download_movenet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1Vhn4QZchP0lZOWODYR-VzfXf-4oMwsE8"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
