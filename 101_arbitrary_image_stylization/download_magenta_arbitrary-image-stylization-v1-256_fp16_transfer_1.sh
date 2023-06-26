1a79f5141b6bfd59e19726473a83e8aa2b58e317
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 22:42:17 +0900

    Google Drive to Wasabi Storage

--- 101_arbitrary_image_stylization/download_magenta_arbitrary-image-stylization-v1-256_fp16_transfer_1.sh
+++ 101_arbitrary_image_stylization/download_magenta_arbitrary-image-stylization-v1-256_fp16_transfer_1.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1K4kcW2mm1JkHG2jBG4I0u5A0r-zwJQQ8"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/101_arbitrary_image_stylization/magenta_arbitrary-image-stylization-v1-256_fp16_transfer_1/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
