9d56a502976abccbd96d4ac8d157c0c113bcdf8b
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 08:50:07 +0900

    Google Drive to Wasabi Storage

--- 072_NanoDet/download.sh
+++ 072_NanoDet/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1oER-UkW0qhzcFRUKCnsRttn7x_KFRn8T"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/072_NanoDet/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
