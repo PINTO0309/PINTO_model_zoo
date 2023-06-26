fbe1b92d237d756f326f27a529ee6dcaa990e9e2
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 22:50:49 +0900

    Google Drive to Wasabi Storage

--- 098_SPICE/download.sh
+++ 098_SPICE/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="17057IKk8aQ5zNH7ApO9FGGjQDMmXSnOq"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/098_SPICE/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
