b7cfdb2674c712d5d24cb0f97260a715ad8c2c0d
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 09:28:53 +0900

    Google Drive to Wasabi Storage

--- 078_MODNet/download.sh
+++ 078_MODNet/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1bs0Tu0Tu_zIqp-ZCVNW1M6M8_c7kw4B3"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/078_MODNet/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
