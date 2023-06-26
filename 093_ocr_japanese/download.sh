4bab08f0c9a0e1b5b470f38b628534bb658917f7
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 23:08:51 +0900

    Google Drive to Wasabi Storage

--- 093_ocr_japanese/download.sh
+++ 093_ocr_japanese/download.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-fileid="11yNWHOg0TdRjT5_fYjTJa8lkJ7ILgYX4"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/093_ocr_japanese/saved_model_detectionnet.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
-fileid="1LlO-L1Hmta5uPrQVZainJslgoGBfw0ex"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/093_ocr_japanese/saved_model_classifiernet.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
