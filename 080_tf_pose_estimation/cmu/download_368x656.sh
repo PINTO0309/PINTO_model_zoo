9f3715b338e615946a8c4833b3a625828e714fcb
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 10:05:57 +0900

    Google Drive to Wasabi Storage

--- 080_tf_pose_estimation/cmu/download_368x656.sh
+++ 080_tf_pose_estimation/cmu/download_368x656.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1V-AD7iO5ZpxXYVwZ2DD2AsrqhY8W2ZF3"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/080_tf_pose_estimation/cmu/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
--- 080_tf_pose_estimation/mobilenet_thin/download_368x432.sh
+++ 080_tf_pose_estimation/mobilenet_thin/download_368x432.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1ac2puxcdQQnG91Ye_oC09Zw_XqZjmQ_I"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/080_tf_pose_estimation/mobilenet_thin/368x432/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
