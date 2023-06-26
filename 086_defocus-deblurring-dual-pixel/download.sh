c1e1960b12e3a7e782527e9afda9a975c6451175
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 09:26:21 +0900

    Google Drive to Wasabi Storage

--- 086_defocus-deblurring-dual-pixel/download.sh
+++ 086_defocus-deblurring-dual-pixel/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="14ShuvvXj1S4fX5is2SLH2vH-0ZhLPg9-"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/086_defocus-deblurring-dual-pixel/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
