cb63b78e00f063850003b5875d4e21ff26e35c77
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 08:52:14 +0900

    Google Drive to Wasabi Storage

--- 071_Noise2Noise/download.sh
+++ 071_Noise2Noise/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="12PbbFQfDMC85sQtkRkNO4fxAIpWWk5GE"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/071_Noise2Noise/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
