2f86b318ac64e0d7bea9f55190cff00ae0ec01fa
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 08:20:37 +0900

    Google Drive to Wasabi Storage

--- 088_mobilenetv3-poseestimation/download.sh
+++ 088_mobilenetv3-poseestimation/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1Tb89Pw0kQE331H5Qlbpt2oRqVUYPM0nA"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/088_mobilenetv3-poseestimation/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
