c6ffe4ba39bb52a7e2c65f9b344381d56916bac5
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 08:54:55 +0900

    Google Drive to Wasabi Storage

--- 070_age-gender-recognition/01_float32/09_saved_model_to_coreml.py
+++
@@ -1,8 +0,0 @@
-### tensorflow==2.3.1
-
-import tensorflow as tf
-import coremltools as ct
-
-mlmodel = ct.convert('saved_model_age', source='tensorflow')
-mlmodel.save("age_gender_recognition_62x62_float32.mlmodel")
-
--- 070_age-gender-recognition/06_tftrt/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1biZXt0f-CTntqi-YqdgG08O9e1PK6vpL"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
