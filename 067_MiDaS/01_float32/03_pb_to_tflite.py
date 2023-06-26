e96187a1baa981a6c82513282d55415c532b95c2
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 09:54:07 +0900

    Google Drive to Wasabi Storage

--- 067_MiDaS/01_float32/03_pb_to_tflite.py
+++
@@ -1,8 +0,0 @@
-### tensorflow==2.3.1
-
-import tensorflow.compat.v1 as tf
-
-converter = tf.lite.TFLiteConverter.from_frozen_graph(
-  'model_float32.pb', ['inputs'], ['Identity'])
-tflite_model = converter.convert()
-open("model_float32.tflite", "wb").write(tflite_model)
--- 067_MiDaS/08_openvino/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="14uRDhddRaXtlV1K83p0CTozdDYi6nu0O"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
