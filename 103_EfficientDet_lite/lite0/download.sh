c2d813b77b562b63e13ac04c1793251c6073ed46
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 22:34:17 +0900

    Google Drive to Wasabi Storage

--- 103_EfficientDet_lite/lite0/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1FjwPWbBND1b4Ppz3GOCox_DtJc69Ubsi"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
