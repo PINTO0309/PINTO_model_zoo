47e7a68e73281873dbc88b3eacbe52d04040762e
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 23:00:55 +0900

    Google Drive to Wasabi Storage

--- 095_centerface/download_1batch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1zm6EJ7OjaF6LnIjBWfH0o54FILhzujZN"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
