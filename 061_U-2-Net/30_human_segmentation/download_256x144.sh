9dd2342d0425f96ea4baed8aa75668901a59b8ea
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-26 23:30:03 +0900

    Google Drive to Wasabi Storage

--- 061_U-2-Net/30_human_segmentation/download_256x144.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1qu0iZ0K11Kwu8bdC9ls47A-y4ROWqkFe"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
