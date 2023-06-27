66c418136e5799f35708503f17ce7e739063f01c
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 10:48:49 +0900

    Google Drive to Wasabi Storage

--- 027_minimal-hand/download.sh
+++ 027_minimal-hand/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/027_minimal-hand/resources.tar.gz" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/041_DBFace/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
