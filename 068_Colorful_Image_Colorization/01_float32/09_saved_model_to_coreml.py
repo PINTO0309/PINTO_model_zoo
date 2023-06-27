32eb0c1944b7085b40ed1fe9a7d5dfa07bf7210f
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 09:01:07 +0900

    Google Drive to Wasabi Storage

--- 068_Colorful_Image_Colorization/01_float32/09_saved_model_to_coreml.py
+++
@@ -1,8 +0,0 @@
-### tensorflow==2.3.1
-
-import tensorflow as tf
-import coremltools as ct
-
-mlmodel = ct.convert('saved_model', source='tensorflow')
-mlmodel.save("colorization_release_v2_256x256_float32.mlmodel")
-
