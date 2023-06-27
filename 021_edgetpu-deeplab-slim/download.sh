4dad13a7aaad7cc405cde602307fcab4cd5b805f
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 10:53:41 +0900

    Google Drive to Wasabi Storage

---
+++ 021_edgetpu-deeplab-slim/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/021_edgetpu-deeplab-slim/resources.tar.gz" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+rm cookie
+
+echo Download finished.
