9da85729bc56057078929bb6290f01fe88ca57f3
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 10:55:46 +0900

    Google Drive to Wasabi Storage

--- 041_DBFace/download.sh
+++ 041_DBFace/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1S8mKC55jTaNSyHs2e0J8BPDia9jd5SSi"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/041_DBFace/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
