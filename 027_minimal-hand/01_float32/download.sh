68b0fcadc62faf39b1aaf1348c7ea35652955b4f
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 10:44:46 +0900

    Google Drive to Wasabi Storage

--- 027_minimal-hand/01_float32/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1R8kMK-R17slm8bS_rj8unVppzGEYI2a7"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
--- 027_minimal-hand/04_float16_quantization/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1a6vY4CNo_Ew2SlG-9jQt2lASW5r5_tPp"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
