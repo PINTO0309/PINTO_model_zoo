a1d15c5c36e0e8a5c154bd4903d33718589a9056
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 09:24:46 +0900

    Google Drive to Wasabi Storage

--- 063_3d-bounding-box-estimation-for-autonomous-driving/03_integer_quantization/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1mg2y7mukVyZjS3YU8Q4g9SnbaoufvX2J"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
