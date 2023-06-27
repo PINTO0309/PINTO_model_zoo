89ab8aa43777c0aa2d5b3644d70a88b60066a186
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 10:27:40 +0900

    Google Drive to Wasabi Storage

--- 059_yolov5/22_yolov5s_new/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="11D7b_D5Ksknc1ZgHHfMG10zvvmgHEa_C"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
