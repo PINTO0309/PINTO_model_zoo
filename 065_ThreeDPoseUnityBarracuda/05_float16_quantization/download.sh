1ff0031c7ca8f47446383f3c8b6e08b3ed3ae440
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 09:06:40 +0900

    Google Drive to Wasabi Storage

--- 065_ThreeDPoseUnityBarracuda/05_float16_quantization/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1ikHjTmrFMagu1v11Rp3iD8ye6_9W-LXh"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
