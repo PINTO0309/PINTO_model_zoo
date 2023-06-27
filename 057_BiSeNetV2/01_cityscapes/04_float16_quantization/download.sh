95e3e69c56fcc3a4463cb9508bf758db515a2059
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 23:54:22 +0900

    Google Drive to Wasabi Storage

--- 057_BiSeNetV2/01_cityscapes/04_float16_quantization/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1J1BgS_V_0ASczJ33X2Jt0JEb92ScjRJi"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
