74210e9ea32afbce160566ca721bbbd5eb5a00be
pinto0309 <rmsdh122@yahoo.co.jp>
2023-06-27 09:27:03 +0900

    Google Drive to Wasabi Storage

--- 062_facial_cartoonization/09_coreml/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1ObUBNoza5ILpixgdpX_f8qwZNM0w2DAR"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
--- 062_facial_cartoonization/10_tftrt/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1Ha07zSmcqjdUj5_hUVq3xrhMNK9Gj4aw"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
