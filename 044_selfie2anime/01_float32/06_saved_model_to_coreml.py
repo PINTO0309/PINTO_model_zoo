d3f32acc437a18dfee3a7f649a1e6db0c19d6479
pinto0309 <rmsdh122@yahoo.co.jp>
2023-07-03 00:12:55 +0900

    Google Drive to Wasabi Storage

--- 044_selfie2anime/01_float32/06_saved_model_to_coreml.py
+++
@@ -1,7 +0,0 @@
-### tensorflow==2.3.0
-
-import tensorflow as tf
-import coremltools as ct
-
-mlmodel = ct.convert('saved_model', source='tensorflow')
-mlmodel.save("selfie2anime_256x256_float32.mlmodel")
