2a4fd0fcc9d1777f7837498fce72816975645d61
pinto0309 <rmsdh122@yahoo.co.jp>
2023-07-02 23:11:15 +0900

    Google Drive to Wasabi Storage

--- 053_BlazePose/10_lite_full_heavy_version_May6_2021/download_heavy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1MPpJN3uDtr2xn55xVGj0eB-nqyKrQGeU"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
