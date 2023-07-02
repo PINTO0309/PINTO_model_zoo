a5198f5dc17b972bed1a67170c7feafbad1616fd
pinto0309 <rmsdh122@yahoo.co.jp>
2023-07-03 00:26:54 +0900

    Google Drive to Wasabi Storage

--- 038_ssdlite_mobiledet_edgetpu/02_weight_quantization/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1r-71Uon9Ka_bA7uYeqDv-un6wZ0I3WDD"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
--- 038_ssdlite_mobiledet_edgetpu/03_integer_quantization/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1BaUiB-yaxu8Xm8_QgoZ_eKhj96iRUwRc"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
