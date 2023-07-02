77d7d402224b703462964aa0fcfc0c5ef4946f11
pinto0309 <rmsdh122@yahoo.co.jp>
2023-07-03 00:16:09 +0900

    Google Drive to Wasabi Storage

--- 043_face_landmark/01_float32/11_saved_model_to_coreml.py
+++
@@ -1,8 +0,0 @@
-### tensorflow==2.3.0
-
-import tensorflow as tf
-import coremltools as ct
-
-mlmodel = ct.convert('saved_model', source='tensorflow')
-mlmodel.save("face_landmark_160x160_float32.mlmodel")
-
