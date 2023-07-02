3b5e2e140fe36442ff7a98b38ce14a72249ff831
pinto0309 <rmsdh122@yahoo.co.jp>
2023-07-03 00:04:52 +0900

    Google Drive to Wasabi Storage

--- 049_iris_landmark/download.sh
+++ 049_iris_landmark/download.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-fileid="1VHM41B8bSr07loNtHlTbXr89670w3H9W"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/049_iris_landmark/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
