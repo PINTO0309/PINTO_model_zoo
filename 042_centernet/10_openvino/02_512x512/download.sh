afff6594352479f1a8ed7ba65b6bdb6606ce3c3d
pinto0309 <rmsdh122@yahoo.co.jp>
2023-07-03 00:19:19 +0900

    Google Drive to Wasabi Storage

--- 042_centernet/10_openvino/02_512x512/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="15p29mjDTEL4PaM0Sa5O8uXHTB2bVyXCy"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
