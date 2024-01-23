c107676ed35a34086652593bdaa0481b53abcba7
pinto0309 <rmsdh122@yahoo.co.jp>
2024-01-24 08:19:42 +0900

    Google Drive to Wasabi Storage

--- 030_BlazeFace/01_float32/download_new.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1DOEH2xS_bGVXEsriNJkKE3my62ZH3FUl"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
--- 030_BlazeFace/07_openvino/download_new.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fileid="1NGZeHXLQc2a5YMqIpAWa6jmjZiXZAuAv"
-html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&`echo ${html}|grep -Po '(confirm=[a-zA-Z0-9\-_]+)'`&id=${fileid}" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
