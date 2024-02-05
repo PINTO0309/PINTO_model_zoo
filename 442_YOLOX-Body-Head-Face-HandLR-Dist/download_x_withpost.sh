bb44207d6f501bdc6e8eac6b9f1a33bc4caa6531
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2024-02-05 20:47:32 +0900

    Update download_x_withpost.sh

--- 442_YOLOX-Body-Head-Face-HandLR-Dist/download_x_withpost.sh
+++ 442_YOLOX-Body-Head-Face-HandLR-Dist/download_x_withpost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/441_YOLOX-Body-Head-Hand-Face-Dist/resources_x_dist_withpost.tar.gz" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/442_YOLOX-Body-Head-Face-HandLR-Dist/resources_x_withpost.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
