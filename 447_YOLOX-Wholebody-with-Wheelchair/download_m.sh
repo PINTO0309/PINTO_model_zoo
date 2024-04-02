aae5d2aad9b9cb19deb2ae94c763517f9253d4c8
pinto0309 <rmsdh122@yahoo.co.jp>
2024-04-02 09:40:04 +0900

    download sh update

--- 447_YOLOX-Wholebody-with-Wheelchair/download_m.sh
+++ 447_YOLOX-Wholebody-with-Wheelchair/download_m.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/445_YOLOX-Body-Head-Face-HandLR-Foot-Dist/resources_m.tar.gz" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/447_YOLOX-Wholebody-with-Wheelchair/resources_m.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
