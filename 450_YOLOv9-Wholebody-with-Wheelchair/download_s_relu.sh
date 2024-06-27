1bba2f05d86cc46b2d71e5be45fe55cc63916633
pinto0309 <rmsdh122@yahoo.co.jp>
2024-06-27 14:39:10 +0900

    YOLOv9

---
+++ 450_YOLOv9-Wholebody-with-Wheelchair/download_s_relu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/450_YOLOv9-Wholebody-with-Wheelchair/resources_s_relu.tar.gz" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
