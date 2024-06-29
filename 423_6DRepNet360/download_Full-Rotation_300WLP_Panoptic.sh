5d7be7e99d8bc655a7b84913a9ef43e934203198
pinto0309 <rmsdh122@yahoo.co.jp>
2024-06-29 21:02:46 +0900

    Full-Rotation_300WLP_Panoptic

---
+++ 423_6DRepNet360/download_Full-Rotation_300WLP_Panoptic.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/423_6DRepNet360/resources_full.tar.gz" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
