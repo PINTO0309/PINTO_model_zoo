6a7e780d08e51f2465921379240126232ca5cc2c
atinfinity <dandelion1124@gmail.com>
2024-07-19 23:39:54 +0900

    fixed download scripts

--- 450_YOLOv9-Wholebody-with-Wheelchair/download_m.sh
+++ 450_YOLOv9-Wholebody-with-Wheelchair/download_m.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/447_YOLOX-Wholebody-with-Wheelchair/resources_m.tar.gz" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/450_YOLOv9-Wholebody-with-Wheelchair/resources_m.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
