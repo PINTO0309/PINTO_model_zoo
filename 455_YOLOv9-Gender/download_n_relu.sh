c8f0e05edd109aae38e00bb804877dd84b1cf628
pinto0309 <rmsdh122@yahoo.co.jp>
2024-08-04 20:59:54 +0900

    Remove ReLU

--- 455_YOLOv9-Gender/download_n_relu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/455_YOLOv9-Gender/resources_n_relu.tar.gz" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
