f2915818e556c275fd5d0920cf5db78983322257
pinto0309 <rmsdh122@yahoo.co.jp>
2024-08-18 17:46:43 +0900

    wip

--- 457_YOLOv9-Wholebody17/download_c.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/457_YOLOv9-Wholebody17/resources_c.tar.gz" -o resources.tar.gz
-tar -zxvf resources.tar.gz
-rm resources.tar.gz
-
-echo Download finished.
