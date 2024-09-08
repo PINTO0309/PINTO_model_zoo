f7452b96df718005e84d008fbde21cfbc17bc7bc
pinto0309 <rmsdh122@yahoo.co.jp>
2024-09-08 14:24:36 +0900

    models update

--- 019_White-box-Cartoonization/download.sh
+++ 019_White-box-Cartoonization/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/019_White-box-Cartoonization/019_White-box-Cartoonization.tar.gz" -o resources.tar.gz
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/019_White-box-Cartoonization/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
 
