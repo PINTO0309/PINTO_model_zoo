0272c501f29fa41e2d020284d47cc298547579db
pinto0309 <rmsdh122@yahoo.co.jp>
2024-10-25 11:27:55 +0900

    tensorflow, tensorflow.js

---
+++ 456_YOLOv9-Wholebody15/download_tensorflowjs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/456_YOLOv9-WholeBody15/resources_tfjs.tar.gz" -o resources.tar.gz
+tar -zxvf resources.tar.gz
+rm resources.tar.gz
+
+echo Download finished.
